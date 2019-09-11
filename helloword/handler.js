e2f39b6e982ef21c27524f834cf59ed5708e1645
brunopenso <viperp@gmail.com>
2019-09-11 06:43:55 -0300

    math path

--- helloword/handler.js
+++ helloword/handler.js
@@ -13,3 +13,34 @@ module.exports.helloWorld = (event, context, callback) => {
 
   callback(null, response);
 };
+
+//event.httpMethod
+//event.headers.<nameOfHeaders>
+//event.path
+//event.pathParameters.<name>
+//event.queryStringParameters
+module.exports.sum = (event, context, callback) => {
+  var num1 = Number(event.pathParameters.num1);
+  var num2 = Number(event.pathParameters.num2);
+
+  var response = null;
+
+  if (!num1 || !num2) {
+    var result = "";
+    if (!num1) {
+      result = "Num1 is invalid"
+    } else if (!num2){
+      result = "Num2 is invalid"
+    }
+    response = {
+      statusCode: 400,
+      body: result
+    }    
+  } else {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({result:num1+num2})
+    }
+  }
+  callback (null, response);
+};
