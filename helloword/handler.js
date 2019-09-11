f8430ce32d59af73e7f7e33e57d8400ed2d23f12
brunopenso <viperp@gmail.com>
2019-09-11 07:07:51 -0300

    divide function added

--- helloword/handler.js
+++ helloword/handler.js
@@ -44,3 +44,42 @@ module.exports.sum = (event, context, callback) => {
   }
   callback (null, response);
 };
+
+module.exports.divide = (event, context, callback) => {
+
+  if (!event.queryStringParameters) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({result: "Query string parameters not found"})
+    }
+    callback (null, response);
+    return;    
+  }
+
+  if (!event.queryStringParameters.p1 ||
+    !event.queryStringParameters.p2) {
+      const response = {
+        statusCode: 400,
+        body: JSON.stringify({result: "Parameters p1 and p2 must be informed"})
+      }
+      callback (null, response);
+      return;    
+  }
+  const n1 = Number(event.queryStringParameters.p1);
+  const n2 = Number(event.queryStringParameters.p2);
+
+  if (!n1 || !n2) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({result: "Parameters p1 and p2 must be number"})
+    }
+    callback (null, response);
+    return;    
+  }
+
+  var response = {
+    statusCode: 200,
+    body: JSON.stringify({result: n1/n2})
+  }
+  callback (null, response);
+};
