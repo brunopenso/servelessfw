ddee51adae0df3f48227e189f77f3fd8bc29a755
brunopenso <viperp@gmail.com>
2019-09-12 06:40:08 -0300

    exception tests working, but not with the ideal solution

--- helloword/business/Calculator.test.js
+++ helloword/business/Calculator.test.js
@@ -4,6 +4,24 @@ test('add 1 + 2 eq 3', () => {
     expect(calc.MethodSum(1,2)).toBe(3);
 });
 
+test('param 1 string', () => {
+    try {
+        calc.MethodSum('a',2)
+        expect(1).toBe(2);
+    } catch (error) {
+        expect(error.toString()).toBe('Error: Invalid Number');
+    }
+}); 
+
 test('param 2 string', () => {
-    expect(calc.MethodSum(1,4)).toThrow('Invalid Number');
+    try {
+        calc.MethodSum(1,'b')
+        expect(1).toBe(2);
+    } catch (error) {
+        expect(error.toString()).toBe('Error: Invalid Number');
+    }
+}); 
+
+test('both string', () => {
+    expect(calc.MethodSum('3','5')).toBe(8);
 }); 
