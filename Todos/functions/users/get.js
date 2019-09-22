bd35e87dbbca72abb8eb1daddfbdbbcf0c1ac741
brunopenso <viperp@gmail.com>
2019-09-22 20:45:47 -0300

    some more methods

--- Todos/functions/users/get.js
+++ Todos/functions/users/get.js
@@ -1,8 +1,9 @@
 'use strict';
 
 module.exports = async event => {
+  
     return {
       statusCode: 200,
-      body: JSON.stringify('GET method')
+      body: JSON.stringify('GET method for id > ' + event.pathParameters.id)
     };
 };
--- Todos/functions/users/put.js
+++ Todos/functions/users/put.js
@@ -1,8 +1,38 @@
 'use strict';
 
+const isString = (obj => typeof obj === 'string' || obj instanceof String)
+
 module.exports = async event => {
+    //console.log(typeof event.body)
+    var params = isString(event.body) ? JSON.parse(event.body) : event.body;
+    //console.log(params);
+
+    //validate event.body against schema
+    if (!params.mail || params.mail === "") {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({"error":"Invalid mail"})
+      };
+    } else if (!params.mail.includes('@')){
+      return {
+        statusCode: 400,
+        body: JSON.stringify({"error":"Invalid mail"})
+      };
+    }
+
+    //check if the email exists
+
+    //create DB structure
+    const dbInfo = {
+      "mail": params.mail,
+      "name": params.name,
+      "birthDate": params.birthDate
+    };
+
+    //save the result
+
+    //return
     return {
-      statusCode: 200,
-      body: JSON.stringify('PUT method')
+      statusCode: 201
     };
 };
